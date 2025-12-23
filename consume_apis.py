46aadf476c10b907d8c617d598a3d386856a2d8a
abdouseye <abdou.gsietechnology.com>
2025-12-23 21:02:32 +0000

    refactor(consommation API Gemini)

--- consume_apis.py
+++ consume_apis.py
@@ -1,4 +1,24 @@
+import os
 import requests
+from dotenv import load_dotenv
+
+# chargement de toutes les variables d'environnement
+load_dotenv()
+
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+
+
+def get_first_five_quotes():
+    url = "https://zenquotes.io/api/quotes"
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json() # liste de cinquante citations
+        print("Données reçues", data)
+        try:
+            return data[:5]
+        except IndexError:
+            return None
+    return None
 
 
 def get_quote_of_the_day():
@@ -6,8 +26,10 @@ def get_quote_of_the_day():
     response = requests.get(url)
     if response.status_code == 200:
         data = response.json()
-        print("Données reçues", data)
-        return data
+        try:
+            return {"q": data[0].get("q"), "a": data[0].get("a")}
+        except IndexError:
+            return None
     return None
 
 
@@ -20,4 +42,24 @@ def get_google_search_result(cle):
         return response.text
     return None
 
-get_google_search_result("animaux")
+def prompt_gemini_api_2_5_flash():
+    url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent"
+    headers = {
+        "x-goog-api-key": GEMINI_API_KEY,
+        "Content-Type": "application/json"
+    }
+    payload = {
+        "contents": [
+        {
+            "parts": [
+            {
+                "text": "Explain how AI works in a few words"
+            }
+            ]
+        }
+        ]
+    }
+    response = requests.post(url, headers=headers, json=payload)
+    print("Réponse retournée par l'API Gemini 2.5 flash ", response.text)
+
+prompt_gemini_api_2_5_flash()
